9411d26d282ad464d83ff6b4ab3a00d68726ba7c
NuhaUbuntu <nuhadinata@gmail.com>
2018-11-27 21:57:46 +0700

    final project

--- index.py
+++ index.py
@@ -1,12 +1,26 @@
+#!/usr/bin/python
+
 from flask import Flask
 from flask import request
 from flask import jsonify
 from flask_cors import CORS
+from sklearn.externals import joblib
 
 app = Flask(__name__)
 CORS(app)
 
+
+
 @app.route("/analyze", methods=['POST'])
 def analyze():
-	attributes = request.data
-	return jsonify(5)
+	attributes = request.get_json()
+
+	#
+	mlp = joblib.load('model.mdl')
+	result = mlp.predict([attributes])[0]
+	#Kode utama disini
+	#
+
+	# Hasil akhir
+	return jsonify(result)
+	
